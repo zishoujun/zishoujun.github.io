a4f63729cefa6852a91239895e994dc35d59036f
子受君 <78142974+zishoujun@users.noreply.github.com>
2024-09-09 16:33:26 +0800

    Add files via upload

---
+++ gril/index.js
@@ -0,0 +1,5 @@
+document.title = "韩语研"
+document.querySelector('.icon').href = "./images/韩语研.jpg"
+document.querySelector('.avatar').src = "./images/韩语研.jpg"
+document.querySelector('.title').innerHTML = document.title
+document.querySelector('.desc').innerHTML = `A girl who loves to laugh is always lucky! `
